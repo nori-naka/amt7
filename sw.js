bcdbc3dd50fd657b136d27da73d38b8bbc0dec18
noriaki.nakamura <noriaki.nakamura@gmail.com>
2019-07-01 20:31:07 +0900

    0.0.3e

--- sw.js
+++ sw.js
@@ -1,24 +1,40 @@
 const CACHE_NAME = 'cache-v1';
 const urlsToCache = [
     './',
-    './index.html',
-    './beacon.js',
-    './exif.js',
-    './flip.js',
-    './map.js',
-    './user_list.js',
-    './rtc.js',
-    './css/ol.css',
-    './css/spectrum.css',
-    './css/style.css',
-    './js/jquery-3.3.1.min.js',
-    './js/jquery.spectrum-ja.js',
-    './js/jquery.tinycolorpicker.min.js',
-    './js/ol.js'
-];
-
-const no_urlsToCache = [
-    '/RECORD_layer/position.json',
+    "./beacon.js",
+    "./css/ol.css",
+    "./css/rtc.css",
+    "./css/spectrum.css",
+    "./css/style.css",
+    "./exif.js",
+    "./favicon.ico",
+    "./flip.js",
+    "./icon-192.png",
+    "./index.html",
+    "./index_wv.html",
+    "./js/jquery.spectrum-ja.js",
+    "./js/jquery.tinycolorpicker.min.js",
+    "./js/jquery-3.3.1.min.js",
+    "./js/ol.js",
+    "./js/spectrum.rev.js",
+    "./main.js",
+    "./manifest.json",
+    "./map.js",
+    "./modal.css",
+    "./modal.js",
+    "./node_modules/",
+    "./pic/",
+    "./RECORD_layer/memo_pin.png",
+    "./RECORD_layer/mic.png",
+    "./RECORD_layer/record.js",
+    "./RECORD_layer/RECORD_layer.js",
+    "./RECORD_layer/speech2text.js",
+    "./RECORD_layer/video_pin.png",
+    "./rhttp.js",
+    "./rhttps.js",
+    "./rtc.js",
+    "./sw.js",
+    "./user_list.js",
 ];
 
 self.addEventListener('install', (event) => {
@@ -50,37 +66,35 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    if (event.request.method == "POST") return;
 
-    const pathname = new URL(event.request.url).pathname
-    no_urlsToCache.forEach((url_str)=>{
-        if (url_str == pathname) return;
-    });
-
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-
-                let fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest)
-                    .then((response) => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        let responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                cache.put(event.request, responseToCache);
-                            });
-
+    if (event.request.url.endsWith('/RECORD_layer/position.json')) {
+        return fetch(event.request);
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then((response) => {
+                    if (response) {
                         return response;
-                    });
-            })
-    );
+                    }
+    
+                    let fetchRequest = event.request.clone();
+    
+                    return fetch(fetchRequest)
+                        .then((response) => {
+                            if (!response || response.status !== 200 || response.type !== 'basic') {
+                                return response;
+                            }
+    
+                            let responseToCache = response.clone();
+    
+                            caches.open(CACHE_NAME)
+                                .then((cache) => {
+                                    cache.put(event.request, responseToCache);
+                                });
+    
+                            return response;
+                        });
+                })
+        );    
+    }
 });
